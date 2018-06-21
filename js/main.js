f9c718e3a3ada6fc6753723f989a8b364ce203cd
mozukraken <mozukraken@gmail.com>
2018-06-22 06:08:03 +0900

    画像をランダムに出力するようにした。

--- js/main.js
+++ js/main.js
@@ -3,11 +3,17 @@
 
   var boxes = document.getElementsByClassName('box');
 
+  var contents = [
+    'coin.png',
+    'empty.png',
+    'cobra.png',
+  ]
+
   function init() {
     var i;
     for (i = 0; i < boxes.length; i++) {
       boxes[i].addEventListener('click', function() {
-        this.src = 'img/coin.png';
+        this.src = 'img/' + contents[Math.floor(Math.random() * contents.length)];
         this.className = 'box';
       });
     }
