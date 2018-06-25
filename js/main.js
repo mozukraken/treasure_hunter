251368dae5cc066231b0c4d4893c4849aed65824
mozukraken <mozukraken@gmail.com>
2018-06-25 23:11:42 +0900

    リスタート機能を完成させた。

--- js/main.js
+++ js/main.js
@@ -14,11 +14,15 @@
     for (i = 0; i < boxes.length; i++) {
       boxes[i].addEventListener('click', function() {
         var j;
+        if (this.className.indexOf('shake') === -1 ) {
+          return;
+        }
         for (j = 0; j < boxes.length; j++) {
           boxes[j].src = 'img/' + contents[Math.floor(Math.random() * contents.length)];
           boxes[j].className = 'box disabled';
         }
         this.className = 'box';
+        document.getElementsById('btn').className = '';
       });
     }
   }
